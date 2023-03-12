d7b9bc87aacb33366c7ccbaf08e1044315a8d6da
Shubham Kamdi <shubhamvkamdi@gmail.com>
2023-03-11 19:15:13 -0800

    Initial Commit

---
+++ Path-NavStack/Path_NavStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Path_NavStackApp.swift
+//  Path-NavStack
+//
+//  Created by Shubham Kamdi on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Path_NavStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
